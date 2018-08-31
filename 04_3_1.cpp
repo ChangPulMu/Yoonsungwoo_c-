11e489caf0c00c296478afc6076db64c84bf1da4
ChangPulMu <16906@naver.com>
2018-08-31 11:06:16 +0900

    윤성우 열혈 C++ Chapter 4 공부 파일들과 OOP 프로젝트 2단계 파일

---
+++ 04_3_1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Point{
+	private:
+		int xpos, ypos;
+	public:
+		Point(int x, int y) :xpos(x), ypos(y) {}
+		void ShowPointInfo() const{
+			cout<<'['<<xpos<<", "<<ypos<<']'<<endl;
+		}
+};
+
+class Circle{
+	private:
+		Point p;
+		int rad;
+	public:
+		Circle(int x, int y, int r) :p(x,y), rad(r) {}
+		int Get_rad() const{
+			return rad;
+		}
+		void ShowCircleInfo() const{
+			cout<<"radius: "<<rad<<endl;
+			p.ShowPointInfo();
+		}
+};
+
+class Ring{
+	private:
+		Circle c1, c2;
+	public:
+		Ring(int x1, int y1, int r1, int x2, int y2, int r2) :c1(x1,y1,r1), c2(x2,y2,r2) {}
+		void ShowRingInfo() const{
+			if(c1.Get_rad()<c2.Get_rad()){
+				cout<<"Inner Circle Info..."<<endl;
+				c1.ShowCircleInfo();
+				cout<<"Outter Circle Info..."<<endl;
+				c2.ShowCircleInfo();
+			}
+			else{
+				cout<<"Inner Circle Info..."<<endl;
+				c2.ShowCircleInfo();
+				cout<<"Outter Circle Info..."<<endl;
+				c1.ShowCircleInfo();
+			}
+		}
+};
+
+int main(void){
+	Ring ring(1,1,4,2,2,9);
+
+	ring.ShowRingInfo();
+
+	return 0;
+}
